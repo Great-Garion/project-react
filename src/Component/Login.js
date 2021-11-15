095115b1e3056df57247d59b1ef6ce5a6f28dd5c
yeyen-13 <yeyentriutami13@gmail.com>
2021-11-15 05:43:23 -0800

    membuat componen Login

---
+++ src/Component/Login.js
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+function Login(){
+    let [user,setUser]= useState({
+        username:"",
+        password:"",
+    })
+
+let handleUser =(e)=>{
+    let{name,value} = e.target
+    setUser({
+        ...user,
+        [name]:value,
+    })
+}
+console.log (user)
+    let [dataUser,setDataUser]=useState([])
+    let handleLogin =(e)=>{
+        e.preventDefsult(S)
+    }
+    useEffect(()=>{
+        fetch("https://618e643350e24d0017ce1267.mockapi.io/user")
+        .then((resp)=> resp.json())
+        .then((data)=>setDataUser(data))
+        console.log(dataUser)
+    },[])
+    localStorage.setItem("Login",dataUser)
+    return(
+        <div></div>
+    )
+
+}
+export default Login
