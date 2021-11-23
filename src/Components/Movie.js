39dd14bc0704b056bb6c88d9c9fb58f14bee3bf8
yeyen-13 <yeyentriutami13@gmail.com>
2021-11-22 23:57:21 -0800

    css login

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -10,6 +10,7 @@ import {
   Row,
   Col,
   Placeholder,
+
 } from "react-bootstrap";
 import Carousel from "react-bootstrap/Carousel";
 function Movie() {
@@ -27,7 +28,47 @@ function Movie() {
 
   return (
     <div style={{ backgroundColor: "#000000c4" }}>
-      <div
+      <Carousel fade>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={movieList.backdrop_path}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={`https://image.tmdb.org/t/p/w500${movieList.backdrop_path}`}
+            alt="Second slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={movieList.backdrop_path}
+            alt="Third slide"
+          />
+
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+
+       <div
         className="d-flex align-content-center flex-wrap bd-highlight example-parent "
         style={{ marginTop: "60px" }}
       >
@@ -79,7 +120,7 @@ function Movie() {
             </div>
           );
         })}
-      </div>
+      </div> 
     </div>
   );
 }
