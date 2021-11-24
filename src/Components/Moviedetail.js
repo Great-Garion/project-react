117260fc43dcd70623d2013ad4cee267eafcd9e7
dellilaaaaa <dellilapellokila@gmail.com>
2021-11-24 12:51:41 +0800

    Perubahan movie detail

--- src/Components/Moviedetail.js
+++ src/Components/Moviedetail.js
@@ -1,63 +1,91 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { Card, Container, Row, Col } from "react-bootstrap";
+
+import { Card,Container,Row,Col } from "react-bootstrap";
 
 function MovieDetail() {
   let { id } = useParams();
   let [movie, setMovie] = useState([]);
   useEffect(() => {
-    axios(`https://api.themoviedb.org/3/movie/${id}?api_key=6591eb1ed775a26d3cfbb3f9fb54272c&language=en-US
-    `).then((rest) => {
+    axios(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=6591eb1ed775a26d3cfbb3f9fb54272c&language=en-US`
+    ).then((rest) => {
       console.log(rest.data);
       setMovie(rest.data);
     });
   }, []);
   console.log(movie);
-  
   return (
-    <div style={{ backgroundColor: "#F5C0C0", height: "119vh", width: "100%" }}>
-      <Container>
-        <Row className="mt-10">
-          <Col xs={6} md={4}>
-            <Card style={{ width: "15rem", margin: "5rem", border: "none" }}>
-              <div>
-                <Card.Img
-                  style={{ width: "15rem", }}
-                  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                  alt="Card image"
-                />
-              </div>
-            </Card>
-          </Col>
-          <Col xs={12} md={8} style={{ marginTop: "5rem" }}>
-            <div>
-              <Card.Title>{movie.original_title}</Card.Title>
-              <Card.Text>{movie.overview}</Card.Text>
-              <Card.Text><b>Status</b> :{movie.status}</Card.Text>
-              <Card.Text><b>Release_date</b> : {movie.release_date}</Card.Text>
-              <Card.Text><b>Popularity</b> : {movie.popularity}</Card.Text>
-              {/* <Card.Text>
-                Original_language : {movie.original_language}
-              </Card.Text> */}
+    <div>
 
-              <Card.Text><b>Tagline </b> : {movie.tagline}</Card.Text>
-              <Card.Text style={{ display: "flex" }}>
-                <b>Genre</b><p className="px-1"> : </p>
-                {movie.genres?.map((item) => {
-                  return <p className="px-1">{item.name}, </p>;
-                })}
-              </Card.Text>
-              {/* <Card.Text style={{ display: "flex" }}>
-                Production_countries:
-                {movie.production_countries?.map((item) => {
-                  return <h6 style={{ paddingLeft: "5px" }}>{item.name},</h6>;
-                })}
-              </Card.Text> */}
-            </div>
-          </Col>
-        </Row>
-      </Container>
+
+<Container>
+  <Row>
+    <Col>
+    <div>
+    <Card.Img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt="Card image"
+        />
+        {/* <Card.ImgOverlay> */}
+    </div>
+   
+      </Col>
+
+    <Col>
+    <div>
+    <Card.Title> Title :  <p>{movie.original_title}</p></Card.Title>
+   
+          <Card.Text>Overview :<p>{movie.overview}</p></Card.Text>
+          
+          <Card.Text>status : </Card.Text>
+          <Card.Text>Release : </Card.Text>
+<p>{movie.release_date}</p>
+          <Card.Text>
+            Genre :
+            {movie.genres?.map((item) => {
+              return <p>{item.name}</p>;
+            })}
+          </Card.Text>
+    </div>
+    </Col>
+  </Row>
+</Container>
+
+
+
+
+
+
+
+
+{/* 
+
+      <Card
+        className="bg-dark text-white"
+        style={{ width: "50%", height: "50%" }}
+      >
+        <Card.Img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt="Card image"
+        />
+        <Card.ImgOverlay>
+          <Card.Title>{movie.original_title}</Card.Title>
+          <Card.Text>{movie.overview}</Card.Text>
+          <Card.Text>status : {movie.status}</Card.Text>
+          <Card.Text>Release : {movie.release_date}</Card.Text>
+          <Card.Text>
+            {" "}
+            Genre :{" "}
+            {movie.genres?.map((item) => {
+              return <h6>{item.name}</h6>;
+            })}
+          </Card.Text>
+        </Card.ImgOverlay>
+
+      </Card> */}
+
     </div>
   );
 }
